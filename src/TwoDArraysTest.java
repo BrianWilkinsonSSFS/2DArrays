920a9d1e83c579084f75b24f22d4e49c5a52efa1
brianwilkinson <brian.wilkinson@ssfs.org>
2020-03-20 07:49:00 -0400

    changed test to 2 days

--- src/TwoDArraysTest.java
+++ src/TwoDArraysTest.java
@@ -22,18 +22,6 @@ public class TwoDArraysTest {
         assertEquals(-11, TwoDArrays.sumAll(arr));
     }
 
-    @Test
-    public void isASquare() {
-        int[][] arr = {{1, 2, 3, 4}, {6, 2, 5, 3}, {9, 0, 1, 7}, {3, 1, 8, 6}};
-
-        assertEquals(true, TwoDArrays.isSquare(arr));
-    }
-
-    @Test
-    public void isNotASquare() {
-        int[][] arr = {{1, 6, 4}, {6, 2, 1}};
-        assertEquals(false, TwoDArrays.isSquare(arr));
-    }
 
     @Test
     public void maxValuePositive() {
